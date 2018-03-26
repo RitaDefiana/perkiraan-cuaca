6b523669943747a35886d28c8b3d769ccf5d9c7e
RitaDefiana <37035695+RitaDefiana@users.noreply.github.com>
2018-03-26 18:31:47 +0700

    perkiraan cuaca

---
+++ App.js
@@ -0,0 +1,12 @@
+import React from 'react';
+import Appweather1615051080 from './src/comp/Appweather1615051080';
+
+//import { StyleSheet, Text, View } from 'react-native';
+
+export default class App extends React.Component {
+  render() {
+    return (
+      <Appweather1615051080 />
+    );
+  }
+}
